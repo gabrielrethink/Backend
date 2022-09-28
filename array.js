09da4d6492dc391f4d5435090a7c19b4bc1ecaf1
Gabriel Gomes <gabriel.gomes@rethink.dev>
2022-09-28 14:22:56 -0300

    initial commit

---
+++ array.js
@@ -0,0 +1,135 @@
+/**
+ * Material de apoio sobre manipulação de arrays em JS
+ */
+
+
+// ============== map ============== //
+
+/**
+ * Para que serve: O método Array.map() normalmente é usado para aplicar algumas alterações aos elementos;
+ */
+
+ const arrayMap = ['Fernando', 'Felipe', 'Ana', 'Fabi', 'Gabriel', 'Lucas', 'Karol', 'Amanda', 'Carol'];
+
+ const mappedArray = arrayMap.map(name => ({
+     nome: name,
+     departamento: "Engenharia",
+     cargo: "Estagiário"
+ }))
+ 
+ console.log("Map: \n",{arrayMap, mappedArray})
+
+ 
+
+// ============== foreach ============== //
+
+/**
+ * Para que serve: 
+ */
+
+const arrayForeach = [];
+
+
+// ============== find ============== //
+
+/**
+ * Para que serve: 
+ */
+
+ const arrayFind = [];
+
+// ============== filter ============== //
+
+/**
+ * Para que serve: 
+ */
+
+ const arrayFilter = [];
+
+// ============== reduce ============== //
+
+/**
+ * Para que serve: 
+ */
+
+ const arrayReduce = [];
+
+// ============== splice ============== //
+
+/**
+ * Para que serve: 
+ */
+
+ const arraySplice = [];
+
+// ============== some ============== //
+
+/**
+ * Para que serve: 
+ */
+
+ const arraySome = [];
+
+// ============== every ============== //
+
+/**
+ * Para que serve: 
+ */
+
+ const arrayEvery = [];
+
+// ============== indexOf ============== //
+
+/**
+ * Para que serve: 
+ */
+
+ const arrayIndexOf = [];
+
+// ============== join ============== //
+
+/**
+ * Para que serve: 
+ */
+
+ const arrayJoin = [];
+
+// ============== sort ============== //
+
+/**
+ * Para que serve: 
+ */
+
+ const arraySort = [];
+
+// ============== shift ============== //
+
+/**
+ * Para que serve: 
+ */
+
+ const arrayShift = [];
+
+// ============== pop ============== //
+
+/**
+ * Para que serve: 
+ */
+
+ const arrayPop = [];
+
+// ============== unshift ============== //
+
+/**
+ * Para que serve: 
+ */
+
+ const arrayUnshift = [];
+
+// ============== push ============== //
+
+/**
+ * Para que serve: 
+ */
+
+ const arrayPush = [];
